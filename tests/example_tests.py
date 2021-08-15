e04a4ff272589956c5837fe65964d02c7e865f99
noidias <noidias1@gmail.com>
2021-08-15 16:34:42 +1200

    simple pytest.bdd example

--- tests/example_tests.py
+++ tests/example_tests.py
@@ -1,5 +1,34 @@
 from pages.main_page import MainPage
 from pages.ican_navigation import IcanNavigationMenu
+from pytest_bdd import scenario, given, when, then
+
+
+@scenario('example_tests.feature', 'user can navigate to the ican page',features_base_dir='features')
+def test_example_is_working(page):
+    print('starting bdd test')
+
+@given('Given a example site')
+def goto_website(page):
+    main_page = MainPage(page)
+    page.goto("https://example.com")
+    main_page.verifiy_header()
+
+@when('When they navigate to the ican website')
+def navigate_to_ican(page):
+    main_page = MainPage(page)
+    main_page.select_more_information_link()
+    ican_navigation = IcanNavigationMenu(page)
+    ican_navigation.verify_menu_items()
+
+@then('Then they are able to select all the menus')
+def check_ican_nav_menu(page):
+    ican_navigation = IcanNavigationMenu(page)
+    ican_navigation.select_domains_link()
+    ican_navigation.select_numbers_link()
+    ican_navigation.select_protocols_link()
+    ican_navigation.select_about_us_link()
+
+
 
 def test_example_is_working(page):
     main_page = MainPage(page)
